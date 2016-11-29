a2261c9383865723d468f4868a806b98d140c04f
jonny <jonny@jonnydeMacBook-Pro.local>
2016-11-29 10:43:19 +0800

    sync

--- animate/mianyang.js
+++ animate/mianyang.js
@@ -2,10 +2,16 @@ var heart = Snap("#heart");
   heart.attr({
   "transform":new Snap.Matrix().scale(0.0,0.0,326,124)
   });
-
-  setTimeout(heartAni,5000);
+var onOff = true;
+  setTimeout(heartAni,6000);
   function heartAni(){
+  	setInterval(function (){
     heart.animate({
-    "transform":new Snap.Matrix().scale(2,2,326,124)
-    },1000,mina.bounce);
+    "transform":new Snap.Matrix().scale(0.5,0.5,326,124)
+    },800,mina.bounce,function (){
+    	heart.animate({
+    		"transform":new Snap.Matrix().scale(2,2,326,124)
+    	},200,mina.bounce)	
+    });
+  	},1000);
   };
