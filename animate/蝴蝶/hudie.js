660623c93e8eaf3e835ac961c81bf84f46d592d7
jonny <jonny@jonnydeMacBook-Pro.local>
2016-11-29 15:54:32 +0800

    sync

--- animate/蝴蝶/hudie.js
+++ animate/蝴蝶/hudie.js
@@ -7,28 +7,28 @@ setInterval(ani,1000);
 ani();
 function ani(){
 	leftF.animate({
-	"transform":new Snap.Matrix().scale(1,0,67.289261,231.7657)
+	"transform":new Snap.Matrix().scale(1,-1,67.289261,231.7657)
 },400,mina.linear,function (){
 	leftF.animate({
 	"transform":new Snap.Matrix().scale(1,1,67.289261,231.7657)
 	},400,mina.linear)	
 });
 rightF.animate({
-	"transform":new Snap.Matrix().scale(1,0,67.289261,231.7657)
+	"transform":new Snap.Matrix().scale(1,-1,67.289261,231.7657)
 },350,mina.linear,function (){
 	rightF.animate({
 	"transform":new Snap.Matrix().scale(1,1,67.289261,231.7657)
 	},350,mina.linear)	
 });
 leftN.animate({
-	"transform":new Snap.Matrix().scale(1,0,67.289261,231.7657)
+	"transform":new Snap.Matrix().scale(1,-1,67.289261,231.7657)
 },500,mina.linear,function (){
 	leftN.animate({
 	"transform":new Snap.Matrix().scale(1,1,67.289261,231.7657)
 	},500,mina.linear)	
 });
 rightN.animate({
-	"transform":new Snap.Matrix().scale(1,0,67.289261,231.7657)
+	"transform":new Snap.Matrix().scale(1,-1,67.289261,231.7657)
 },450,mina.linear,function (){
 	rightN.animate({
 	"transform":new Snap.Matrix().scale(1,1,67.289261,231.7657)
